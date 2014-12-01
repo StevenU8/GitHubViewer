75a762b47298170d3c7a0c172d224b3ae9ede913
Steven U <Steven@u-mail.org>
2014-11-30 21:46:25 -0800

    Learning about interval service

--- GitHubViewer/script.js
+++ GitHubViewer/script.js
@@ -5,7 +5,7 @@
 
     var app = angular.module("githubViewer", []);
 
-    var MainController = function($scope, $http) {
+    var MainController = function($scope, $http, $interval, $Log) {
 
         var onUserComplete = function(response) {
             $scope.user = response.data;
@@ -20,6 +20,17 @@
         var onError = function(reason) {
             $scope.error = "Could not fetch the user";
         }
+        
+        var decrementCountdown = function() {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search();
+            }
+        }
+
+        var startCountdown = function() {
+            $interval(decrementCountdown, 1000, $scope.countdown);
+        }
 
         $scope.search = function() {
             $http.get("https://api.github.com/users/" + $scope.username)
@@ -29,8 +40,10 @@
         $scope.username = "angular";
         $scope.message = "GitHub Viewer";
         $scope.repoSortOrder = "-stargazers_count";
+        $scope.countdown = 5;
+        startCountdown();
     };
 
-    app.controller("MainController", ["$scope", "$http", MainController]);
+    app.controller("MainController", MainController);
 
 }());
